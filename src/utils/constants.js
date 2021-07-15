4d28d5e6c42c2814538935b3cb10caaf3be4d92a
Tim Häggqvist Luotomäki <tim.haggqvist@gmail.com>
2021-07-15 19:26:11 +1000

    still on wrong branch lol

---
+++ src/utils/constants.js
@@ -0,0 +1,15 @@
+export const ACTIONS = {
+        ADD_ITEM: "ADD_ITEM",
+        TOGGLE_ITEM: "TOGGLE_ITEM",
+        SET_FILTER: "SET_FILTER",
+        DELETE_ITEM: "DELETE_ITEM",
+        DELETE_ALL: "DELETE_ALL"
+};
+
+export const VISIBILITY_FILTERS = {
+        ALL: "all",
+        REMAINING: "remaining",
+        COMPLETED: "completed"
+};
+
+export const TODO_LIST_STORAGE = 'TODO_LIST_STORAGE';
